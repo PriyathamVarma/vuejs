803fb52874a2c370b18685ad6b29f8a6ce0f0a66
Varma <varma@quickfiredigital.com>
2021-12-22 12:46:47 +0000

    after list rendering

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ const app = Vue.createApp({
             product: 'socks with colorful stripes',
             image: './assets/images/socks_blue.jpeg',
             stock: true,
-            inventory: 100
+            inventory: 100,
+            details: ['50% cotton','40% wool','20% polyester'],
+            variants: [
+                {id : 1, color : 'green'},
+                {id : 2, color : 'blue'},
+            ]
         }
     }
 });
