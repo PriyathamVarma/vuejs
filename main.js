2324ea87fb3cce9131ddec41e912b2547085c917
Varma <varma@quickfiredigital.com>
2021-12-22 15:28:43 +0000

    after computed properties

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ const app = Vue.createApp({
     data(){
         return { 
             product: 'socks with colorful stripes',
-            image: './assets/images/socks_blue.jpeg',
+            selectedVariant: 0,
             stock: true,
             inventory: 100,
             details: ['50% cotton','40% wool','20% polyester'],
             variants: [
-                {id : 1, color : 'green',image: './assets/images/socks_green.jpeg'},
-                {id : 2, color : 'blue',image: './assets/images/socks_blue.jpeg'},
+                {id : 1, color : '#2A5038',image: './assets/images/socks_green.jpeg', qty:50},
+                {id : 2, color : '#022F3B',image: './assets/images/socks_blue.jpeg', qty:10},
             ],
             cart : 10000
         }
@@ -23,8 +23,40 @@ const app = Vue.createApp({
         cartFunc(){
                 this.cart = 0 ;
         },
+
         updateImage(variantImage){
             this.image = variantImage;
+        },
+        
+        addToCart() {
+            this.cart = this.cart+1;
+            this.inventory = this.inventory-1;
+        },
+
+        removeFromCart(){
+            this.cart = this.cart-1;
+            this.inventory = this.inventory+1;
+        },
+        emptyCart(){
+            this.cart = 0;
+            this.inventory = 100;
+        },
+        updateVariant(index){
+            this.selectedVariant = index;
+            console.log(index);
+        }
+    },
+
+    //computed properties
+    computed:{
+        title(){
+            return this.product + ' ' + this.product;
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
+        },
+        qty(){
+            return this.variants[this.selectedVariant].qty;
         }
     }
     
